99f38de845949618fe8fee76c917dbef800e2716
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-01-08 12:44:32 -0300

    Sección 8 finalizada - día 7

--- Curso iOS y Swift 5.7.playground/Pages/Sección 8 - Swift avanzado.xcplaygroundpage/Contents.swift
+++ Curso iOS y Swift 5.7.playground/Pages/Sección 8 - Swift avanzado.xcplaygroundpage/Contents.swift
@@ -146,3 +146,266 @@ do {
 
 
 
+//MARK: - Type casting (Validación de tipos de datos)
+//Llegado el momento, puede que nosotros nos encontremos con un dato y querramos averiguar si pertenece a un tipo de dato en concreto. Para eso utilizaremos el Type Casting
+let myStringName = "Jacob"
+let myInt = 27
+
+class MyClass {
+    var name: String! // con el símbolo "!" obligamos a que siempre tenga el dato. No puede ser nulo
+    var age: Int!
+}
+
+let myClass = MyClass()
+myClass.name = "Jacob Aguilar"
+myClass.age = 27
+
+let myArray: [Any] = [myStringName, myInt, myClass] //Con el tipo de dato [Any] indicamos que el arreglo puede tener cualquier tipo de dato
+
+for item in myArray {
+    //*Type Casting para saber que tipo de dato contiene nuestro arreglo*
+    if item is String {
+        //*Downcasting* (Transformación de un tipo de dato a otro más restringido, ya que todos los tipos de datos en Swift, se encuentran dentro de una super clase llamada "any", por lo que este dato los transformamos a String)
+        let myItemString = item as! String //Si no ponemos el símbolo "!" Xcode nos arrojará un error, debido a que tenmos que obligar a que sea de tipo string.
+        print("Item es de tipo string y tiene el valor \(myItemString)")
+    } else if item is Int {
+        let myItemInt = item as! Int
+        print("Item es de tipo int y tiene el valor \(myItemInt)")
+    } else if item is MyClass {
+        let myItemMyClass = item as! MyClass
+        print("tem es de tipo string y tiene el valor \(myItemMyClass.name) y \(myItemMyClass.age)") //Despues de "name" o "age" podemos ponder el símbolo "!" para indicar que estos datos, como los conocemos, no son opcionales
+    }
+}
+
+
+//Manera rápida de realizar Type y downcasting
+for item in myArray {
+    if let myItemString = item as? String {
+        print("Item es de tipo string y tiene el valor \(myItemString)")
+    } else if let myItemInt = item as? Int {
+        print("Item es de tipo string y tiene el valor \(myItemInt)")
+    } else if let MyItemMyClass = item as? MyClass {
+        print("tem es de tipo string y tiene el valor \(MyItemMyClass.name!) y \(MyItemMyClass.age!)") //Forzamos a que no sea nulo
+    }
+}
+
+
+
+//MARK: - Tipos anidados (Nested types)
+
+/* Entendamos los tipos anidados como una forma más compleja de crear los enum. La utilidad más habitual de los enum es que nosotros los creemos en una clase para hacer comprobaciones que ya tenemos definidas como valores de un Enum. En el caso de que tengamos que crear enumeraciones para hacer comprobaciones más exhaustivas, es cuando utilizaremos los tipos anidados, que vendrán a ser agrupaciones de enum con funcionalidad muy concreta de ese tipo de enumeración  */
+
+//Ajedrez
+
+struct PiezaAjedrez {
+    let color: Color
+    let type: TipoPieza
+    
+    enum Color: String{
+        case white = "Blanca", black = "Negra"
+    }
+    
+    enum TipoPieza: String {
+        case king = "Rey", queen = "Reina", rock = "Torre" , knight = "Caballo", bishop = "Alfil", pawn = "Peón"
+        
+        struct NumPiezas {
+            let number: Int
+        }
+        
+        var number: NumPiezas { //Numero de piezas por cada pieza
+            switch self {
+            case .king:
+                return NumPiezas(number: 1)
+            case .queen:
+                return NumPiezas(number: 1)
+            case .rock:
+                return NumPiezas(number: 2)
+            case .knight:
+                return NumPiezas(number: 2)
+            case .bishop:
+                return NumPiezas(number: 2)
+            case .pawn:
+                return NumPiezas(number: 8)
+            }
+        }
+    }
+    
+    var descrip: String {
+        if type.number.number == 1 {
+            return "Hay \(type.number.number) pieza de ajedrez, es de color \(color.rawValue) y de tipo \(type.rawValue)"
+        }
+        
+        return "Hay \(type.number.number) piezas de ajedrez, es de color \(color.rawValue) y de tipo \(type.rawValue)"
+    }
+}
+
+let myPiece = PiezaAjedrez(color: .black, type: .rock)
+print(myPiece.descrip)
+
+
+
+//MARK: - Extensions
+/*
+ Las extensiones es una característica que tiene Swift. Y nos servirá para agregar una nueva funcionalidad a una clase, a una estructura, a una enumeración o a un protocolo existente.
+ 
+ Lo que podemos hacer con una extensión es añadir características a nivel de tipo de dato.
+ 
+ De esta forma añadiremos una caractrística nueva y todos los datos de ese tipo pasarán a poseerla. Podemos agregar por ejemplo: La definición de propiedad de distancia, definición de funciones, nuevos inicializadores, sub índices, tipos de datos anidados o hacer que cualquier tipo se adapte a un protocolo.
+*/
+
+let myMeters: Double = 5000
+
+func metersToKm(meters:Double) -> Double {
+    return meters / 1000
+}
+
+print("\(metersToKm(meters: myMeters)) Kilómetros")
+
+extension Double { /*En este caso agregamos una extensión a la constante myMeters. Dentro de esta extensión agregamos las propiedades  */
+    var km: Double {
+        return self  / 1000  /*Self siempre hace refencia al lugar donde estamos, en este caso hace referencia a que tenemos sun tipo de dato DOUBLE y que myMeters lo estamos dividiendo en mil*/
+    }
+    
+    var m: Double {
+        return self //Podemos retornar a sí mismo que en este caso es myMeters
+    }
+    
+    var cm: Double {
+        return self * 100 // Self retorna que se va a devolver a sí mismo (myMeters) * 100
+    }
+}
+
+print ("\(myMeters.km) Kilómetros")
+print("\(myMeters.cm) Centímetros")
+
+
+
+
+//MARK: - Protocols
+/*
+ Un protocolo nos servirá para definir un modelo de métodos, propiedades y otros requisitos para que una clase, una estructura o una enumeración se adapten a ellos. Un protocolo va a obligar va a obligar a la clase, estructura o enumeración a que adopen una serie de propiedades y funciones a que conforme una serie de propiedades que va tener que implementar
+*/
+
+
+//Creación del protocolo
+protocol PersonProtocol {
+    var name: String { get set } //Con get indicamos que esta variable se puede obtener y con set indicamos que se puede otorgar un valor
+    var age: Int { get set }
+    
+    func fullName() -> String
+}
+                
+//Implementación del protocolo en estructura Programmer
+struct Programmer: PersonProtocol {
+    var name: String
+    var age: Int
+    var lenguaje: String
+    
+    func fullName() -> String {
+           return "El nombre es: \(name), edad: \(age) y su lenguaje favorito es: \(lenguaje)"
+    }
+}             
+
+//Implementación del protocolo para la estructura Teacher
+struct Teacher: PersonProtocol {
+    var name: String
+    var age: Int
+    var asignatura:String
+    
+    func fullName() -> String {
+        return "El nombre es: \(name), edad: \(age) y y dicta la asignatura de: \(asignatura)"
+    }
+}
+
+let myProgrammer = Programmer(name: "Jacob", age: 27, lenguaje: "Swift")
+let myTeacher = Teacher(name: "Juan", age: 27, asignatura: "Matemática")
+
+print (myProgrammer.fullName())
+print(myTeacher.fullName())
+
+
+
+//MARK: - Protocolo delegado
+/*
+ Este tipo de protocolo lo tendremos que utilizar muy habitualmente en Swift, sobre todo cuando se comience con la programación de iOS, porque es el organismo que nos permitirá comunicar dos clases en sentido inverso
+*/
+
+class FirstClass: SecondClassProtocol {
+    func callSecond() {
+        let secondClass = SecondClass()
+        secondClass.delegate = self
+        secondClass.callFirst()
+    }
+    
+    func call() { //Método que requiere el protocolo
+        print ("He vuelto")
+    }
+}
+
+protocol SecondClassProtocol {
+    func call()
+}
+
+class SecondClass {
+    var delegate: SecondClassProtocol?
+    
+    func callFirst() {
+        sleep(2)
+        delegate?.call()
+    }
+}
+
+let firstClass = FirstClass()
+firstClass.callSecond()
+
+
+
+//MARK: - Generics
+/*
+ Un código genérico, básicamente, nos permitirá escribir funciones y tipos flexibles que son reutilizables y que van a funcionar con diferentes tipos que se adapten a unos requisitos. De esta manera vamos a poder abstraer nuestro código y evitar la duplicación de operaciones.
+*/
+
+
+//Intercambio sin genéricos
+func intercambioDeEnteros(a: inout Int, b: inout Int) { //En los parámetros de entrada, inout indica que esos parámetros van a entrar y salir sin que los retornemos directamente nosotros.
+    let temporalA = a
+    a = b
+    b = temporalA
+}
+
+var myFirstInt = 5
+var mySecondInt = 10
+print("El primer valor es \(myFirstInt) y el segundo valor es \(mySecondInt)")
+intercambioDeEnteros(a: &myFirstInt, b: &mySecondInt)
+print("El primer valor es \(myFirstInt) y el segundo valor es \(mySecondInt)")//Aquí vemos como se cambiaron los valores
+
+
+
+
+//Intercambio con genéricos
+func intercambioDeGenericos<T>(a: inout T, b: inout T) { // Con "<T>" indicamos que pueden ingresar datos genéricos, de cualquier tipo
+    let temporalA = a
+       a = b
+       b = temporalA
+}
+
+var myFirstGeneric = "Jacob"
+var mySecondGeneric = "Aguilar"
+print("El primer valor es \(myFirstGeneric) y el segundo valor es \(mySecondGeneric)")
+intercambioDeGenericos(a: &myFirstGeneric, b: &mySecondGeneric)
+print("El primer valor es \(myFirstGeneric) y el segundo valor es \(mySecondGeneric)")
+
+
+
+//MARK: - Precedencia y Asocietividad
+//Swift siempre hará las operaciones por precedencia de peso, divisiones y multiplicaciones primero, luego sumas y finalmente restas
+//let myInt: Int = 2 + 3 % 4 * 5 // Resultado: 17 (Primero la división, luego la multiplicación y luego la suma)
+let myInt: Int = (2 + ((3 % 4) * 5)) //Realizando la misma operación pero con asocietividad (nosotros le indicamos que operación hacer mediante paréntesis)
+let myInt2: Int = (((2 + 3) % 4) * 5) //Resultado: 5 - Aquí le indicamos que primero haga la suma, luego la división y por último la multiplicación
+
+//Más operadores avanzados
+// - Operadores de bit a bit: Para realizar operacione con bits de tipo lógico con NOT. AND, OR, XOR
+// - Operadores de desplazamiento: Para desplazar bits
+// - Operadores de desbordamiento: Para manejar errores en valores por encima de su límite
+// - Operadores custom: Para definir nuestros propios operadores
+
