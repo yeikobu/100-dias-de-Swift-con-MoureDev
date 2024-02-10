309640a13cb31ff103f1fb2d1eed8c684f4ad212
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-02-10 15:15:37 -0300

    Sección 13 - Finalizada

---
+++ Sección 13 Auto Layaut y Constrains parte 2/Sección 13 Auto Layaut y Constrains parte 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sección 13 Auto Layaut y Constrains parte 2
+//
+//  Created by Jacob Aguilar on 10-02-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
