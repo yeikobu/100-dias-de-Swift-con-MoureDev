96a16cb97c89c6ef28bccdf4e8a121d258a3cdef
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-02-13 14:45:49 -0300

    Sección 14 - parte 2

--- Sección 14 - Vistas y controles de navegación/Sección 14 - Vistas y controles de navegación/ViewController.swift
+++ Sección 14 - Vistas y controles de navegación/Sección 14 - Vistas y controles de navegación/ViewController.swift
@@ -6,16 +6,23 @@
 //
 
 import UIKit
+import WebKit
+import MapKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
     @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var myFakeView: UIView!
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var myMap: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myWebView.load(URLRequest(url: URL(string: "https://google.com")!))
+        myMap.centerCoordinate = CLLocationCoordinate2D(latitude: 2.60, longitude: -8.31)
     }
 
     @IBAction func myButtonAction(_ sender: Any) {
