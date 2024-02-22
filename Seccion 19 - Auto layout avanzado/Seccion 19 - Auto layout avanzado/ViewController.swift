4b667a03f37bd8fdc590d49134317802abda2dce
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-02-22 18:23:07 -0300

    Seccion 19 - Parte 1 - Auto layout avanzado

---
+++ Seccion 19 - Auto layout avanzado/Seccion 19 - Auto layout avanzado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seccion 19 - Auto layout avanzado
+//
+//  Created by Jacob Aguilar on 22-02-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
