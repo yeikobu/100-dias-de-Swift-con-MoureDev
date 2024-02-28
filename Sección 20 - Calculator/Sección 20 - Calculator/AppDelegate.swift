ca7742cf04ac9b0957b1ae415cf379910074c165
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-02-28 13:05:59 -0300

    Sección 20 - parte 1 - Crear app para iPhone

---
+++ Sección 20 - Calculator/Sección 20 - Calculator/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  Sección 20 - Calculator
+//
+//  Created by Jacob Aguilar on 28-02-24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        //Setup
+        setupView()
+        
+        return true
+    }
+    
+
+//MARK: - Private methods
+    private func setupView() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+    
+}
+
---
+++ Sección 20 - Calculator/Sección 20 - Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  Sección 20 - Calculator
+//
+//  Created by Jacob Aguilar on 28-02-24.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    //MARK: - Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
